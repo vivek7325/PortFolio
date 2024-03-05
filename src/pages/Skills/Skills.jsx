405cab44dd848b91d5124226c400522aa2064fc8
vivek7325 <68550321+vivek7325@users.noreply.github.com>
2024-03-05 22:42:50 +0530

    Update Skills.jsx

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -9,7 +9,7 @@ const Skills = () => {
       <h2 className='section__title'>Skills</h2>
       <ul className='skills__list'>
         {skills.map((skill) => (
-          <li key={uniqid()} className='skills__list-item btn btn--plain'>
+          <li className='skills__list-item btn btn--plain'>
             {skill}
           </li>
         ))}
